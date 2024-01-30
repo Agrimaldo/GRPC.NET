76205fd325b3868813a14146205483812e8b8223
Agrimaldo Werlei Mendes Brandão <agrimaltr00@hotmail.com>
2024-01-30 20:44:47 -0300

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+
+namespace GrpcClient 
+{ 
+    class Program 
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Wait a few seconds...");
+            Thread.Sleep(500);
+            using (GrpcChannel channel = GrpcChannel.ForAddress("http://localhost:5001"))
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              { 
+                var clientGreeter = new Greeter.GreeterClient(channel);
+                var reply = await clientGreeter.SayHelloAsync(new HelloRequest { Name = "Aqui no Client Side!" });
+                Console.WriteLine($"Server has returned : {reply.Message}");
+
+                var clientCalculator = new Calculator.CalculatorClient(channel);
+                var replyCalc = await clientCalculator.AdditionAsync(new ValuesRequest { ValueOne = 10, ValueTwo = 5 });
+
+                Console.WriteLine($"10 + 5 = {replyCalc.Result} -- response with delay ");
+
+            }
+
+            Console.ReadKey();
+        }
+    }    
+}
+
---
+++ GrpcServer/Services/CalculatorService.cs
@@ -0,0 +1,44 @@
+﻿using Grpc.Core;
+using GrpcServer;
+
+namespace GrpcServer.Services
+{
+    public class CalculatorService : Calculator.CalculatorBase
+    {
+        private readonly ILogger<CalculatorService> _logger;
+        public CalculatorService(ILogger<CalculatorService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<ResultReply> Addition(ValuesRequest request, ServerCallContext context)
+        {
+            Thread.Sleep(5000);
+            return Task.FromResult(new ResultReply
+            {
+                Result = request.ValueOne + request.ValueTwo
+            });
+        }
+        public override Task<ResultReply> Subtraction(ValuesRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResultReply
+            {
+                Result = request.ValueOne - request.ValueTwo
+            });
+        }
+        public override Task<ResultReply> Division(ValuesRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResultReply
+            {
+                Result = request.ValueOne / request.ValueTwo
+            });
+        }
+        public override Task<ResultReply> Multiplication(ValuesRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ResultReply
+            {
+                Result = request.ValueOne * request.ValueTwo
+            });
+        }
+
+    }
+}
